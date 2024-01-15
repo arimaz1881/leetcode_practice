f50282bc559e69360577e06c42356f03edfea421
Zamira <104966750+arimaz1881@users.noreply.github.com>
2024-01-15 23:45:24 +0600

    added recursive method of btree traversal

---
+++ btree_inorder/recursive.go
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func inorderTraversal(root *TreeNode) []int {
+    result := []int{}
+    if root != nil {
+        result = append(result, inorderTraversal(root.Left)...)
+        result = append(result, root.Val)
+        result = append(result, inorderTraversal(root.Right)...)
+    }
+
+    return result
+}
